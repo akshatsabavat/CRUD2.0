be9c7aa29980152ba6218d9220bda70fc7b780e0
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-10-25 18:14:16 +0530

    get and post methods added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,34 @@
 const express = require("express");
+const User = require("../models/userModel");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hi this is the users dashboard");
+router.get("/", async (req, res) => {
+  try {
+    const students = await User.find();
+    res.status(200).json({
+      students,
+    });
+  } catch (err) {
+    res.status(404).json({ errMessage: err.message });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const userName = req.body.userName;
+  const userID = req.body.userID;
+  const userBranch = req.body.userBranch;
+  const student = new User({
+    userName,
+    userID,
+    userBranch,
+  });
+
+  try {
+    const newStudent = await student.save();
+    res.status(200).json(newStudent);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 module.exports = router;
